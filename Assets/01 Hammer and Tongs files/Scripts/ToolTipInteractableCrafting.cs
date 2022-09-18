58871450bb1c2d5f13024f43e8bab12d1abea568
lemonLimeBitta <88814126+lemonLimeBitta@users.noreply.github.com>
2022-09-18 11:19:39 +1000

    0.01.13
    Updated gfx of smelting  UI

---
+++ Assets/01 Hammer and Tongs files/Scripts/ToolTipInteractableCrafting.cs
@@ -0,0 +1,39 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using System;
+using UnityEngine.EventSystems;
+using TMPro;
+
+public class ToolTipInteractableCrafting : MonoBehaviour, IPointerEnterHandler, IPointerExitHandler
+{
+
+    private TextMeshProUGUI descriptionText;
+    [SerializeField]
+    private string ItemDescription;
+
+    [SerializeField]
+    private Transform toolTip;
+
+
+    private void Start()
+    {
+       // itemCostText = toolTip.GetChild(0).GetChild(0).GetComponent<TextMeshProUGUI>();
+        descriptionText = toolTip.GetChild(0).GetComponent<TextMeshProUGUI>();
+    }
+    public void OnPointerEnter(PointerEventData eventData)
+    {
+        //this will bring up the tool tip and update the text to tell us the cost. 
+        toolTip.gameObject.SetActive(true);
+        toolTip.GetComponent<RectTransform>().anchoredPosition = new Vector2(this.transform.parent.GetComponent<RectTransform>().anchoredPosition.x, this.transform.parent.GetComponent<RectTransform>().anchoredPosition.y+120) ;//this.gameObject.transform.GetComponent<RectTransform>().anchoredPosition;
+        descriptionText.text = (string)ItemDescription;
+       // itemCostText.text = (string)itemCost.ToString();
+    }
+
+    public void OnPointerExit(PointerEventData eventData)
+    {
+        //this will close the tool tip and clear the text
+        toolTip.gameObject.SetActive(false);
+    }
+}
